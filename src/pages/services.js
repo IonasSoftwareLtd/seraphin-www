c254b8eb300c7e95942214a2b7861d2897954ec5
Szymon <spc16670@gmail.com>
2022-01-23 07:46:43 +0000

    Add content to Services

--- src/pages/services.js
+++ src/pages/services.js
@@ -39,51 +39,56 @@ export default function Services() {
       <Panel>
         <ContentWrapper>
           <ServiceList className="product-features">
-            <Service
-              header="Consulting services"
-              icon={<RecordVoiceOverIcon />}
-            >
-              We are open to any consultation ranging from possible existing
-              solutions to bespoke product design.
-            </Service>
             <Service header="Product customization" icon={<EditIcon />}>
-              We offer our partners and customers close cooperation to help the
-              designed monitoring system meet any specific expectations.
+              We are open to discussion with our partners and customers about their monitoring needs. 
+              We offer a wide range of approved devices, but we also have the expertise and know-how 
+              to design and manufacture bespoke sensor products. Bring your idea into reality with 
+              our experienced team. We will help you protect your valuable inventory assisting with 
+              the deployment of our Seraphin Platform Monitoring solution in a swift and professional manner.
             </Service>
 
             <Service
               header="Installation of system components"
               icon={<SettingsInputComponentIcon />}
             >
-              Professional installation of your/our monitoring system assuring
-              best signal range for undisturbed communication between system
-              components.
+              We provide installation services of our monitoring system, assuring the best signal 
+              range for undisturbed communication between system components. We guarantee our devices 
+              to be positioned without disturbing of existing setups. It’s only a matter of minutes for 
+              our skilled engineers to set up your system and configure Seraphin Platform to watch and 
+              protect your assets. 
+
             </Service>
             <Service header="Training programs" icon={<PeopleIcon />}>
-              We provide to our partners theoretical and practical trainings for
-              better understanding of the concept and functionality of the
-              system.
+            We provide trainings to our partners for better understanding of our software and hardware products. 
+            These trainings aim to give you a fundamental knowledge of how to manage your system, change parameters 
+            and get access to readings from your sensors using the Seraphin portal. We also provide installation 
+            and calibration trainings for partners willing to take full control over their monitoring system.
             </Service>
 
             <Service
               header="Maintenance and support"
               icon={<SupportAgentIcon />}
             >
-              We help keep your system up and running, so you can ensure
-              important data is delivered on schedule.
+              We will keep your system up and running, so you can ensure important data is delivered on schedule 
+              and all your assets remain protected. Our engineers are by your side, ready to help whenever 
+              support is needed. We guarantee fast response times and swift resolution to any 
+              problem you may encounter.
             </Service>
             <Service
               header="Annual calibration services"
               icon={<PhonelinkSetupIcon />}
             >
-              High quality regular calibration of your/our system essential to
-              ensure the best possible accuracy.
+              High quality, regular calibration of sensors is essential to ensure the best 
+              possible accuracy. We offer annual service of your system involving 
+              UKAS calibration, battery checks, signal range optimisation and full 
+              health checks of all system components.
             </Service>
 
             <Service header="Software update" icon={<UpdateIcon />}>
-              We guarantee the latest software installed on all of the
-              components of your/our system to ensure most efficient and safe
-              data logging.
+            We guarantee the latest software installed on all of the components of your system to 
+            ensure most efficient and safe data logging. Our engineers will notify you whenever 
+            new hardware updates are released and provide the system update without
+            distraction of your monitoring system.  
             </Service>
           </ServiceList>
         </ContentWrapper>
