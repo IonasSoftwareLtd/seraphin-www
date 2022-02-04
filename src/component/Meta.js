292a273457be6511f29859eece0d5a5e4b3f0d5d
Armin Solecki <armin.solecki@gmail.com>
2022-02-04 16:35:16 +0000

    Refactor social

--- src/component/Meta.js
+++ src/component/Meta.js
@@ -1,7 +1,10 @@
 import React from "react";
 import Helmet from "react-helmet";
 
-export function Meta({ title }) {
+export function Meta({
+  title,
+  description = "Seraphin is specialising in wireless sensor cloud monitoring systems based in Glasgow, UK.",
+}) {
   return (
     <Helmet>
       <title>
@@ -9,20 +12,14 @@ export function Meta({ title }) {
           ? `${title} | Seraphin | Wireless sensor cloud monitoring system`
           : "Seraphin | Wireless sensor cloud monitoring system | Glasgow, Uk"}
       </title>
-      <meta
-        name="description"
-        content="Seraphin is specialising in wireless sensor cloud monitoring systems based in Glasgow, UK."
-      />
+      <meta name="description" content={description} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
 
       <meta
         name="og:image"
         content="https://seraphin.cloud/images/social.jpg"
       />
-      <meta
-        name="og:description"
-        content="Seraphin is specialising in wireless sensor cloud monitoring systems based in Glasgow, UK."
-      />
+      <meta name="og:description" content={description} />
       <meta name="og:url" content="https://seraphin.cloud/" />
       <meta name="og:site_name" content="Seraphin" />
 
@@ -31,10 +28,7 @@ export function Meta({ title }) {
         name="twitter:title"
         content="Seraphin | Wireless sensor cloud monitoring system"
       />
-      <meta
-        name="twitter:description"
-        content="Seraphin is specialising in wireless sensor cloud monitoring systems based in Glasgow, UK."
-      />
+      <meta name="twitter:description" content={description} />
       <meta
         name="twitter:image:src"
         content="https://seraphin.cloud/images/social.jpg"
--- src/component/PageLayout.js
+++ src/component/PageLayout.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Meta } from "./Meta";
 import { PageFooter } from "./PageFooter";
 
-export function PageLayout({ children, title, pageHeader }) {
+export function PageLayout({ children, title, description, pageHeader }) {
   return (
     <div className="page" id="outer-container">
-      <Meta title={title} />
+      <Meta title={title} description={description} />
       {pageHeader}
       {/* <PageHeader /> */}
-      <div className="page__content" id="page-wrap">{children}</div>
+      <div className="page__content" id="page-wrap">
+        {children}
+      </div>
       <PageFooter />
     </div>
   );
