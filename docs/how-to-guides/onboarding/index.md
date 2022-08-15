8caefea8218ba8e33acee302de59de73080b8959
Szymon <spc16670@gmail.com>
2022-08-15 07:53:39 +0100

    Try Hugo

--- docs/how-to-guides/onboarding/index.md
+++
@@ -1,6 +0,0 @@
-# User onboarding
-
-Users are usually onboarded onto the system by receiving an invitation link over email.
-
-The email will be issued by the administrator of the Seraphin platform at your site.
-
--- src/component/ContentWrapper.js
+++
@@ -1,6 +0,0 @@
-import CN from "classnames";
-import React from "react";
-
-export function ContentWrapper({ children, className }) {
-  return <div className={CN("content-wrapper", className)}>{children}</div>;
-}
